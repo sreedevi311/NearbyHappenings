ca336485241fbd817774e16f2af0d7d6c7a0f83b
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-08-02 23:42:15 +0530

    Update google.js

--- Backend/oAuth/google.js
+++ Backend/oAuth/google.js
@@ -1,36 +1,31 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const User = require('../models/user.model')
-
-const isProduction = process.env.NODE_ENV === 'production'
-const baseBackendURL = isProduction
-  ? 'https://nearbyhappenings.onrender.com'
-  : 'http://localhost:5000'
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user.model');
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${baseBackendURL}/nearby-happenings/auth/google/callback`
+  callbackURL: 'https://nearbyhappenings.onrender.com/nearby-happenings/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
-    const email = profile.emails[0].value
-    let user = await User.findOne({ email })
+    const email = profile.emails[0].value;
+    let user = await User.findOne({ email });
 
     if (user) {
       if (!user.googleId) {
-        user.googleId = profile.id
-        await user.save()
+        user.googleId = profile.id;
+        await user.save();
       }
     } else {
       user = await User.create({
         googleId: profile.id,
         email,
         name: profile.displayName
-      })
+      });
     }
 
-    return done(null, user)
+    return done(null, user);
   } catch (err) {
-    return done(err, null)
+    return done(err, null);
   }
-}))
+}));
