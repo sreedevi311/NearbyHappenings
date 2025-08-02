b0037f32e0815b09df3142cc8eec12788a80bd56
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-08-02 21:13:50 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/nearby-happenings',
+  baseURL: 'https://nearbyhappenings.onrender.com',
   withCredentials: true
 });
