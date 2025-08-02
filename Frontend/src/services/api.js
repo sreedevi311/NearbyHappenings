b3277cf338a475117786c5ad6fef1c59aaf51efe
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-08-02 21:21:37 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,6 @@
-// src/services/api.js
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://nearbyhappenings.onrender.com/nearby-happenings',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
