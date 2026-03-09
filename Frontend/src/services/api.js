c90d42345f0f1e1e48107fd9e8ac824ca4f55bac
sreedevi311 <venupulaparthi1968@gmail.com>
2026-03-09 21:06:21 +0530

    fixed backend call api in api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/nearby-happenings',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
